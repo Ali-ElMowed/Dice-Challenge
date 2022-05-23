f547d591d1292d37ecaae3b1b44de6b404b1dfa4
Your NameAliAlMowed <you@example.comalmowed.ali@gmail.com>
2022-05-23 10:48:32 +0300

    Adding the conditions to change the images into it is correct numbers for both player 1 and 2

--- script.js
+++ script.js
@@ -11,6 +11,44 @@ window.onbeforeunload = game()
 function game(){
     randomOne = Math.floor(Math.random()*6)+1;
     randomTwo = Math.floor(Math.random()*6)+1;
+    if (randomOne==1){
+        img1.src="images/dice1.png";
+    }
+    else if(randomOne==2){
+        img1.src="images/dice2.png";
+    }
+    else if(randomOne==3){
+        img1.src="images/dice3.png";
+    }
+    else if(randomOne==4){
+        img1.src="images/dice4.png";
+    }
+    else if(randomOne==5){
+        img1.src="images/dice5.png";
+    }
+    else if(randomOne==6){
+        img1.src="images/dice6.png";
+    }
+
+    if (randomTwo==1){
+        img2.src="images/dice1.png";
+    }
+    else if(randomTwo==2){
+        img2.src="images/dice2.png";
+    }
+    else if(randomTwo==3){
+        img2.src="images/dice3.png";
+    }
+    else if(randomTwo==4){
+        img2.src="images/dice4.png";
+    }
+    else if(randomTwo==5){
+        img2.src="images/dice5.png";
+    }
+    else if(randomTwo==6){
+        img2.src="images/dice6.png";
+    }
+
     
     
 
