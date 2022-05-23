41ceab32894f76d9b9b64b8e6d1187b87f7c19a0
Your NameAliAlMowed <you@example.comalmowed.ali@gmail.com>
2022-05-23 10:20:09 +0300

    Adding the variables

---
+++ script.js
@@ -0,0 +1,7 @@
+var mainText = document.getElementsByTagName("h1")
+var img1 = document.getElementsByClassName("img1")[0];
+var img2 = document.getElementsByClassName("img2")[0];
+
+var randomOne = 0;
+var randomTwo = 0;
+
