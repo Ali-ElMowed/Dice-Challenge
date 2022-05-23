0d3be136564181fd12a230366e70c593159aad51
Your NameAliAlMowed <you@example.comalmowed.ali@gmail.com>
2022-05-23 10:47:00 +0300

    Changes the font because the main font causes some problems with chrome , Adding the random numbers

--- script.js
+++ script.js
@@ -1,9 +1,18 @@
-var mainText = document.getElementsByTagName("h1")
+
+var mainText = document.getElementsByTagName("h1")[0];
 var img1 = document.getElementsByClassName("img1")[0];
 var img2 = document.getElementsByClassName("img2")[0];
 
 var randomOne = 0;
 var randomTwo = 0;
 
-document.addEventListener("load",game);
+//document.addEventListener('load',game)
+window.onbeforeunload = game() 
+function game(){
+    randomOne = Math.floor(Math.random()*6)+1;
+    randomTwo = Math.floor(Math.random()*6)+1;
+    
+    
 
+
+}
